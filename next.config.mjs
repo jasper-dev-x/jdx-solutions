57b16b72c43b0e204ec0397bc2d06fc3999e5eef
jasper-dev-x <jasper.dev.x@gmail.com>
2024-08-20 22:00:16 -0400

    Redirect bug fix attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [{type: "host", value: "www.jdx.solutions"}],
+      destination: "https://jdx.solutions/:path*",
+      permanent: true,
+    },
+  ],
+};
 
 export default nextConfig;
